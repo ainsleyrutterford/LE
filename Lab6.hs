bf0f52e543e227f6a94ccccded92d45f2db3e76c
Ainsley Rutterford <ar16478@my.bristol.ac.uk>
2018-05-10 18:20:19 +0100

    updated Lab6

--- Lab6.hs
+++ Lab6.hs
@@ -17,8 +17,9 @@ instance Applicative Maybe' where
   -- pure :: a -> Maybe a
   pure x = Just' x
   -- (<*>) :: Maybe (a -> b) -> Maybe a -> Maybe b
-  Nothing' <*> m = Nothing'
-  Just' f <*> mx = f <$> mx
+  Just' f <*> Just' x = Just' (f x)
+  _ <*> Nothing' = Nothing'
+  Nothing' <*> _ = Nothing'
 
 -- 2 (a).
 -- pure id <*> v = Just id <*> v
@@ -62,15 +63,27 @@ instance Functor List where
 
 -- 2.
 
+(<$) :: a -> List b -> List a
+(<$) = fmap . const
+
 -- 3.
 instance Applicative List where
   -- pure :: a -> List a
   pure x = Cons x Empty
   -- (<*>) :: List (a -> b) -> List a -> List b
-  Empty <*> l = Empty
-  Cons f fs <*> Empty = Empty
-  -- Cons f fs <*> Cons x xs = Cons (f x) (fs <*> xs) -- mappend fmap -- fix this
-  Cons f fs <*> xs = (fmap f xs) <> (fs <*> xs) -- is this correct?
+  Empty <*> _ = Empty
+  _ <*> Empty = Empty
+  Cons f fs <*> xs = (fmap f xs) <> (fs <*> xs)
+
+-- 4.
+
+(<*) :: List a -> List b -> List a
+la <* lb = const <$> la <*> lb
+
+-- 5.
+
+(*>) :: List a -> List b -> List b
+la *> lb = flip const <$> la <*> lb
 
 -- 6.
 instance Alternative List where
@@ -78,28 +91,32 @@ instance Alternative List where
   empty = Empty
   -- (<|>) :: List a -> List a -> List a
   Empty <|> l = l
-  Cons x xs <|> l = Cons x xs
+  x <|> y = x <> y
 
 -- 7 (a). Cons :: a -> List a -> List a
 
--- (b). Cons <$> :: List ((a -> b) -> List a -> List b) -> List ((a -> b) -> List a -> List b)
+-- (b). Cons <$> :: Functor f => f a -> f (list a -> List a)
 
 -- (c). <$ :: Functor f => a -> f b -> f a
 
--- (d). Cons <$ :: Functor f => List (a -> List b -> List a)
---                           -> List (a -> List b -> List a)
+-- (d). Cons <$ :: Functor f => f b -> f (a -> List a -> List a)
 
--- (e). Cons <$> Just 3 <*> Just (Cons 2 Empty) :: Maybe (List a)
+-- (e). Cons <$> Just 3 <*> Just (Cons 2 Empty) :: Maybe (List Int)
 
--- (f). Cons <$> Just 3 :: Maybe (List a -> List a)
+-- (f). Cons <$> Just 3 :: Maybe (List Int -> List Int)
 
--- (g). Cons <$> (Cons 5 Empty) :: List (List a -> List a)
+-- (g). Cons <$> (Cons 5 Empty) :: List (List Int -> List Int)
 
--- (h). Cons 3 <$>
+-- (h). Cons 3 <$> :: Functor f => f (List Int) -> f (List Int)
 
--- 8.
+-- 8. (a). Just (Cons 3 (Cons 2 Empty))
+
+-- (b). Just (Cons 3 (Cons 1 Empty))
+
+-- (c). Just 2
+
+-- (d). Cons (Just 5) (Cons (Just 2) Empty)
 
 -- 9.
 liftATwo :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
 liftATwo f x y = f <$> x <*> y
--- Not sure why?
